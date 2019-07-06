24fc8ac6de0ec543f0d9f883add06c01dfa51b04
ALi816GX <854501715@qq.com>
2019-07-06 16:13:09 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
--- 插入记录  
+-- 插入记录 
+INSERT INTO 表 VALUES(x1,x2,xn); 
 
--- 修改记录  
+-- 修改记录
+UPDATE 表名 SET 字段1=值1，字段2=值2，字段n=值n WHERE 条件;
 
 --  删除记录  
+DELETE FROM 表名 [WHERE 条件];
 
 -- 查询记录  
+SELECT 字段名 FROM 表名;
